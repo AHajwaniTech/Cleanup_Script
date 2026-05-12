a53a2f1db0bf4591f53b6b365e5ed3cae0470fef
Adnan Hajwani <adnan.hajwani.tech@gmail.com>
2026-05-12 19:05:17 +0530

    Added Header To Code

--- cleanup_stage_logs.sh
+++ cleanup_stage_logs.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+##################################################################################
+# Script Name: cleanup_stage_logs.sh
+# Developer: Adnan Hajwani
+# Description: To cleanup batch code which are generated during 11:30 Batch Cron
+# --------------------------------------------------------------------------------
+# Modification Log:
+# --------------------------------------------------------------------------------
+# Date         Modified By          Description
+# ----------   ------------------   ----------------------------------------------
+#                AH08052026         
+##################################################################################
 
 rm -f /home/pipewayweb/log/batch_r_cc_dp_stage*.log 2>/dev/null
